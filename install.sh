a6290b8804ab2a61d097bf1bb6835b44980561f3
AccEDUSON <lesha.melnikov.2000@mail.ru>
2026-06-15 04:45:23 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get update
+apt-get install -y build-essential libz-dev libjpeg-dev libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk
+pip install -r requirements.txt
